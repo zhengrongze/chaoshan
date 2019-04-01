4c6c20e4bc4059d96808df7ae36ec05015944264
13670342436@163.com <13670342436@163.com>
2019-04-02 00:48:43 +0800

    测试

--- other/src/main/java/com/mc/constant/SmsConstant.java
+++ other/src/main/java/com/mc/constant/SmsConstant.java
@@ -1,6 +1,7 @@
 package com.mc.constant;
 
-public class SmsConstant {
+    public class SmsConstant {
     public static final String SIGN_NAME = "IG二队";
     public static final String SMS_CODE = "SMS_162522390";
+    public static final String ddasd = "SMS_162522390";
 }
