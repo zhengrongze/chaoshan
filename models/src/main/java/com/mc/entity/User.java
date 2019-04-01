0902baa91a0fd6de78822a307a64ea6cdc7880d7
13670342436@163.com <13670342436@163.com>
2019-04-02 01:04:39 +0800

    修改错误文件

--- models/src/main/java/com/mc/entity/User.java
+++ models/src/main/java/com/mc/entity/User.java
@@ -3,22 +3,16 @@ package com.mc.entity;
 
 import lombok.Data;
 
-<<<<<<< HEAD
-=======
 /**
  * @ClassName User
  * @Author ZhengRongZe
  * @Date 2018/7/25 21:51
  **/
->>>>>>> a54e1a8d53334c72395b986158e43b73a1ee0191
 @Data
 public class User{
     private Integer id;
     private String username;
     private String password;
-<<<<<<< HEAD
-=======
     private Company company;
    
->>>>>>> a54e1a8d53334c72395b986158e43b73a1ee0191
 }
